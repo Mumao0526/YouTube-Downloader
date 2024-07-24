fbcd6baf6154b54f6f6f1883892ccc04c1c06f1e
Mumao0526 <apple338824@gmail.com>
2024-07-24 21:14:40 +0800

    more options

--- downloader.py
+++ downloader.py
@@ -34,8 +34,7 @@ def onProgress(stream, chunk, remains):
     print(f"Downloading… {percent:05.2f}", end="\r")
 
 
-def toMp3(path: str, output_path: str):
-    yt = YouTube(path, on_progress_callback=onProgress)
+def toMp3(yt: YouTube, output_path: str):
 
     print(yt.title + "\tdownload start")
     # get audio and save to .mp3
@@ -46,19 +45,48 @@ def toMp3(path: str, output_path: str):
     print("OK")
 
 
-def toMp4(path: str, output_path: str):
-    yt = YouTube(path, on_progress_callback=onProgress)
-
+def toMp4(yt: YouTube, output_path: str, resolution: str = None):
     print(yt.title + "\tdownload start")
-    # get HIGHEST resolution video and save to .mp4
-    yt.streams.filter().get_highest_resolution().download(
-        filename=clean_filename(yt.title) + ".mp4", output_path=output_path
-    )
+
+    if resolution:
+        # get video with specified resolution and save to .mp4
+        try:
+            yt.streams.filter(res=resolution).first().download(
+                filename=clean_filename(yt.title) + ".mp4", output_path=output_path
+            )
+        except Exception:
+            print(
+                f"Resolution {resolution} is not available, download the highest resolution."
+            )
+            yt.streams.filter().order_by("resolution")[-1].download(
+                filename=clean_filename(yt.title) + ".mp4", output_path=output_path
+            )
+    else:
+        # get HIGHEST resolution video and save to .mp4
+        # BUG: yt.streams.filter().get_highest_resolution().download(
+        #     filename=clean_filename(yt.title) + ".mp4", output_path=output_path
+        # )
+        yt.streams.filter().order_by("resolution")[-1].download(
+            filename=clean_filename(yt.title) + ".mp4", output_path=output_path
+        )
     print()
     print("OK")
 
 
-def run(link: str, format: str = ".mp4", output_path: str = current_directory):
+def information(yt: YouTube):
+    print(yt.title)
+    print(yt.streams)
+    print()
+
+
+def run(
+    link: str,
+    format: str = ".mp4",
+    output_path: str = current_directory,
+    info: bool = False,
+    resolution: str = None,
+    oauth: bool = False,
+):
     playlist = []
     if "playlist?" in link:  # check link is or not playlist link
         playlist = Playlist(link).video_urls
@@ -67,23 +95,46 @@ def run(link: str, format: str = ".mp4", output_path: str = current_directory):
 
     if format.lower() == ".mp3":
         for songlink in playlist:
-            toMp3(songlink, output_path)
+            yt = YouTube(
+                songlink,
+                on_progress_callback=onProgress,
+                use_oauth=oauth,
+                allow_oauth_cache=oauth,
+            )
+            if info:
+                information(yt)
+            toMp3(yt, output_path)
     if format.lower() == ".mp4":
         for songlink in playlist:
-            toMp4(songlink, output_path)
+            yt = YouTube(
+                songlink,
+                on_progress_callback=onProgress,
+                use_oauth=oauth,
+                allow_oauth_cache=oauth,
+            )
+            if info:
+                information(yt)
+            toMp4(yt, output_path, resolution)
 
 
 def parse_opt():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--link", type=str, required=True,
-                        help="Youtube video link")
+    parser.add_argument("--link", type=str, required=True, help="Youtube video link")
     parser.add_argument("--format", default=".mp4", help=".mp4 or .mp3")
     parser.add_argument(
         "--output_path",
         default=None,
         help="Specifies the path where you want to save files.",
     )
+    parser.add_argument("--info", default=False, help="Show video information")
+    parser.add_argument(
+        "--resolution",
+        default=None,
+        help="Specifies the resolution of the video you want to download. '720p' or '1080p' etc.",
+    )
+    parser.add_argument("--oauth", default=False, action="store_true", help="Use OAuth")
+
     return parser.parse_args()
 
 
